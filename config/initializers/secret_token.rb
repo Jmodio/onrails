7dc3a8ca36aa157f617dd571ea36e639a2be99f2
Joe Amodio <jamodi20@gmail.com>
2013-03-13 16:52:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onrails::Application.config.secret_token = '5b677aaa48a94d72428e540b43e31b9dfec17127474d74d4a426454b67bbddbd9f9164a1f137b623f3f99c2228547f6c17703b1036a233f80682a9dd8a18d438'
